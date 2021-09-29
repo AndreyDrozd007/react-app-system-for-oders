581a11fc91279f6065f083df3db95ae3c2b85ba1
Andrey Drozd <drozdandrew58@gmail.com>
2021-09-29 16:34:56 +0300

    feat: add redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducerForm from '../src/reducers/reducer'
 import './index.css';
 import App from './App.js'
 
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={createStore(reducerForm)}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
